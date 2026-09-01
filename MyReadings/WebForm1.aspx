7cf9766177c82a0fb885adf8c6cb007859e206b9
jalalhasan77 <Yaqout_sky@hotmail.com>
2026-09-01 20:11:20 +0300

    I added Iran Won the War but May Lose the Peace

--- MyReadings/WebForm1.aspx
+++ MyReadings/WebForm1.aspx
@@ -5,10 +5,173 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style type="text/css">
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        .layout-table {
+            width: 100%;
+            table-layout: fixed;
+            border-collapse: collapse;
+        }
+        .layout-table td {
+            vertical-align: top;
+            border: 1px solid #ccc;
+            padding: 8px;
+        }
+        .col-third {
+            width: 33.33%;
+        }
+        .ListBoxLeft {
+            width: 100%;
+            height: 400px;
+        }
+        .form-row {
+            display: flex;
+            align-items: center;
+            margin-bottom: 10px;
+        }
+        .form-label {
+            width: 110px;
+            flex: 0 0 110px;
+            font-weight: bold;
+            padding-right: 8px;
+            text-align: right;
+        }
+        .form-control {
+            flex: 1;
+            max-width: 400px;
+        }
+        .form-control textarea,
+        .form-control input[type="text"] {
+            width: 100%;
+            box-sizing: border-box;
+            border: 1px solid #aaa;
+            border-radius: 6px;
+            padding: 6px 10px;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
+            <table class="layout-table">
+                <tr>
+                    <td class="col-third">
+                        <asp:ListBox ID="ListBox1" runat="server" CssClass="ListBoxLeft"
+                            AutoPostBack="true" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged"></asp:ListBox>
+                    </td>
+                    <td>
+                        <asp:RadioButtonList ID="RadioButtonList1" runat="server" RepeatDirection="Horizontal"
+                            AutoPostBack="true" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged">
+                            <asp:ListItem Text="Title" Value="Title"></asp:ListItem>
+                            <asp:ListItem Text="Title Breaf" Value="Title Breaf"></asp:ListItem>
+                            <asp:ListItem Text="figure" Value="figure"></asp:ListItem>
+                            <asp:ListItem Text="author" Value="author"></asp:ListItem>
+                            <asp:ListItem Text="Paragraph" Value="Paragraph"></asp:ListItem>
+                            <asp:ListItem Text="author-bio" Value="author-bio"></asp:ListItem>
+                        </asp:RadioButtonList>
+
+                        <br />
+
+                        <asp:Panel ID="pnlTitle" runat="server" Visible="false">
+                            <div class="form-row">
+                                <asp:Label runat="server" CssClass="form-label" AssociatedControlID="Titled" Text="Titled:"></asp:Label>
+                                <div class="form-control">
+                                    <asp:TextBox ID="Titled" runat="server"></asp:TextBox>
+                                </div>
+                            </div>
+                        </asp:Panel>
+
+                        <asp:Panel ID="pnlTitleBreaf" runat="server" Visible="false">
+                            <div class="form-row">
+                                <asp:Label runat="server" CssClass="form-label" AssociatedControlID="Breaf" Text="Breaf:"></asp:Label>
+                                <div class="form-control">
+                                    <asp:TextBox ID="Breaf" runat="server" TextMode="MultiLine" Rows="4"></asp:TextBox>
+                                </div>
+                            </div>
+                        </asp:Panel>
+
+                        <asp:Panel ID="pnlFigure" runat="server" Visible="false">
+                            <div class="form-row">
+                                <asp:Label runat="server" CssClass="form-label" AssociatedControlID="src" Text="src:"></asp:Label>
+                                <div class="form-control">
+                                    <asp:TextBox ID="src" runat="server"></asp:TextBox>
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <asp:Label runat="server" CssClass="form-label" AssociatedControlID="alt" Text="alt:"></asp:Label>
+                                <div class="form-control">
+                                    <asp:TextBox ID="alt" runat="server" TextMode="MultiLine" Rows="3"></asp:TextBox>
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <asp:Label runat="server" CssClass="form-label" AssociatedControlID="figcaption" Text="figcaption:"></asp:Label>
+                                <div class="form-control">
+                                    <asp:TextBox ID="figcaption" runat="server" TextMode="MultiLine" Rows="3"></asp:TextBox>
+                                </div>
+                            </div>
+                        </asp:Panel>
+
+                        <asp:Panel ID="pnlAuthor" runat="server" Visible="false">
+                            <div class="form-row">
+                                <asp:Label runat="server" CssClass="form-label" AssociatedControlID="Auther" Text="Auther:"></asp:Label>
+                                <div class="form-control">
+                                    <asp:TextBox ID="Auther" runat="server"></asp:TextBox>
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <asp:Label runat="server" CssClass="form-label" AssociatedControlID="dateline" Text="dateline:"></asp:Label>
+                                <div class="form-control">
+                                    <asp:TextBox ID="dateline" runat="server"></asp:TextBox>
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <asp:Label runat="server" CssClass="form-label" AssociatedControlID="Publine" Text="Publine:"></asp:Label>
+                                <div class="form-control">
+                                    <asp:TextBox ID="Publine" runat="server"></asp:TextBox>
+                                </div>
+                            </div>
+                        </asp:Panel>
+
+                        <asp:Panel ID="pnlParagraph" runat="server" Visible="false">
+                            <div class="form-row">
+                                <asp:Label runat="server" CssClass="form-label" AssociatedControlID="paragraph" Text="paragraph:"></asp:Label>
+                                <div class="form-control">
+                                    <asp:TextBox ID="paragraph" runat="server" TextMode="MultiLine" Rows="4"></asp:TextBox>
+                                </div>
+                            </div>
+                        </asp:Panel>
+
+                        <asp:Panel ID="pnlAuthorBio" runat="server" Visible="false">
+                            <div class="form-row">
+                                <asp:Label runat="server" CssClass="form-label" Text="Auther:"></asp:Label>
+                                <div class="form-control">
+                                    <asp:Label ID="LabelAutherBio" runat="server"></asp:Label>
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <asp:Label runat="server" CssClass="form-label" AssociatedControlID="Info" Text="Info:"></asp:Label>
+                                <div class="form-control">
+                                    <asp:TextBox ID="Info" runat="server"></asp:TextBox>
+                                </div>
+                            </div>
+                        </asp:Panel>
+
+                        <br />
+
+                        <asp:Button ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" style="text-align: center;">
+                        <asp:Button ID="Button1" runat="server" Text="Write HTML" OnClick="Button1_Click" />
+                        <br />
+                        <asp:Label ID="lblStatus" runat="server"></asp:Label>
+                    </td>
+                </tr>
+            </table>
         </div>
     </form>
 </body>
--- MyReadings/WebForm1.aspx.vb
+++ MyReadings/WebForm1.aspx.vb
@@ -1,8 +1,287 @@
 ﻿Public Class WebForm1
     Inherits System.Web.UI.Page
 
+    Private Const FIELD_SEP As String = "|~|"
+
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
 
+    Protected Sub RadioButtonList1_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles RadioButtonList1.SelectedIndexChanged
+        ShowSelectedPanel()
+    End Sub
+
+    Private Sub ShowSelectedPanel()
+        pnlTitle.Visible = False
+        pnlTitleBreaf.Visible = False
+        pnlFigure.Visible = False
+        pnlAuthor.Visible = False
+        pnlParagraph.Visible = False
+        pnlAuthorBio.Visible = False
+
+        Select Case RadioButtonList1.SelectedValue
+            Case "Title"
+                pnlTitle.Visible = True
+            Case "Title Breaf"
+                pnlTitleBreaf.Visible = True
+            Case "figure"
+                pnlFigure.Visible = True
+            Case "author"
+                pnlAuthor.Visible = True
+            Case "Paragraph"
+                pnlParagraph.Visible = True
+            Case "author-bio"
+                LabelAutherBio.Text = Auther.Text
+                pnlAuthorBio.Visible = True
+        End Select
+    End Sub
+
+    Protected Sub btnInsert_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnInsert.Click
+        If RadioButtonList1.SelectedItem Is Nothing Then
+            Exit Sub
+        End If
+
+        Dim selectedValue As String = RadioButtonList1.SelectedValue
+        Dim firstComponentText As String = String.Empty
+        Dim itemValue As String = String.Empty
+
+        Select Case selectedValue
+            Case "Title"
+                firstComponentText = Titled.Text
+                itemValue = selectedValue & FIELD_SEP & Titled.Text
+                Titled.Text = String.Empty
+
+            Case "Title Breaf"
+                firstComponentText = Breaf.Text
+                itemValue = selectedValue & FIELD_SEP & Breaf.Text
+                Breaf.Text = String.Empty
+
+            Case "figure"
+                firstComponentText = src.Text
+                itemValue = selectedValue & FIELD_SEP & src.Text & FIELD_SEP & alt.Text & FIELD_SEP & figcaption.Text
+                src.Text = String.Empty
+                alt.Text = String.Empty
+                figcaption.Text = String.Empty
+
+            Case "author"
+                firstComponentText = Auther.Text
+                itemValue = selectedValue & FIELD_SEP & Auther.Text & FIELD_SEP & dateline.Text & FIELD_SEP & Publine.Text
+                ' Note: Auther is intentionally NOT cleared here -
+                ' its value is reused later to populate the author-bio label.
+                dateline.Text = String.Empty
+                Publine.Text = String.Empty
+
+            Case "Paragraph"
+                Dim ParagraphArr() As String
+                ParagraphArr = Split(paragraph.Text, vbCrLf)
+
+                For Each str As String In ParagraphArr
+                    If Trim(str) = "" Then Continue For
+                    Dim lineValue As String = selectedValue & FIELD_SEP & str
+                    ListBox1.Items.Add(New ListItem(selectedValue & ":" & GetFirstWords(str, 5), lineValue))
+                Next
+
+                paragraph.Text = String.Empty
+
+            Case "author-bio"
+                firstComponentText = LabelAutherBio.Text
+                itemValue = selectedValue & FIELD_SEP & LabelAutherBio.Text & FIELD_SEP & Info.Text
+                Info.Text = String.Empty
+        End Select
+
+        If selectedValue <> "Paragraph" Then
+            ListBox1.Items.Add(New ListItem(selectedValue & ":" & GetFirstWords(firstComponentText, 5), itemValue))
+        End If
+
+    End Sub
+
+    Protected Sub ListBox1_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles ListBox1.SelectedIndexChanged
+        If ListBox1.SelectedItem Is Nothing Then
+            Exit Sub
+        End If
+
+        Dim parts() As String = ListBox1.SelectedItem.Value.Split(New String() {FIELD_SEP}, StringSplitOptions.None)
+        Dim itemType As String = parts(0)
+
+        If RadioButtonList1.Items.FindByValue(itemType) Is Nothing Then
+            Exit Sub
+        End If
+
+        RadioButtonList1.SelectedValue = itemType
+        ShowSelectedPanel()
+
+        Select Case itemType
+            Case "Title"
+                Titled.Text = GetPart(parts, 1)
+
+            Case "Title Breaf"
+                Breaf.Text = GetPart(parts, 1)
+
+            Case "figure"
+                src.Text = GetPart(parts, 1)
+                alt.Text = GetPart(parts, 2)
+                figcaption.Text = GetPart(parts, 3)
+
+            Case "author"
+                Auther.Text = GetPart(parts, 1)
+                dateline.Text = GetPart(parts, 2)
+                Publine.Text = GetPart(parts, 3)
+
+            Case "Paragraph"
+                paragraph.Text = GetPart(parts, 1)
+
+            Case "author-bio"
+                LabelAutherBio.Text = GetPart(parts, 1)
+                Info.Text = GetPart(parts, 2)
+        End Select
+    End Sub
+
+    Private Function GetPart(ByVal parts() As String, ByVal index As Integer) As String
+        If index < parts.Length Then
+            Return parts(index)
+        End If
+        Return String.Empty
+    End Function
+
+    Private Function GetFirstWords(ByVal text As String, ByVal wordCount As Integer) As String
+        If String.IsNullOrWhiteSpace(text) Then
+            Return String.Empty
+        End If
+
+        Dim words() As String = text.Trim().Split(New Char() {" "c}, StringSplitOptions.RemoveEmptyEntries)
+        Dim takeCount As Integer = Math.Min(wordCount, words.Length)
+        Return String.Join(" ", words, 0, takeCount)
+    End Function
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sb As New System.Text.StringBuilder()
+        Dim i As Integer = 0
+        Dim documentTitleText As String = String.Empty
+
+        ' Pre-scan to find the index of the last "Paragraph" item, so we know
+        ' where to close the article-body div once we reach it below.
+        Dim lastParagraphIndex As Integer = -1
+        For idx As Integer = 0 To ListBox1.Items.Count - 1
+            Dim scanParts() As String = ListBox1.Items(idx).Value.Split(New String() {FIELD_SEP}, StringSplitOptions.None)
+            If scanParts(0) = "Paragraph" Then
+                lastParagraphIndex = idx
+            End If
+        Next
+
+        Dim articleBodyOpened As Boolean = False
+
+        While i < ListBox1.Items.Count
+            Dim item As ListItem = ListBox1.Items(i)
+            Dim parts() As String = item.Value.Split(New String() {FIELD_SEP}, StringSplitOptions.None)
+            Dim itemType As String = parts(0)
+
+            Select Case itemType
+                Case "Title"
+                    Dim titledText As String = GetPart(parts, 1)
+                    Dim brafText As String = String.Empty
+
+                    ' If the very next item is the matching Title Breaf, pull its text in
+                    ' and skip it so it isn't processed again on its own.
+                    If i + 1 < ListBox1.Items.Count Then
+                        Dim nextParts() As String = ListBox1.Items(i + 1).Value.Split(New String() {FIELD_SEP}, StringSplitOptions.None)
+                        If nextParts(0) = "Title Breaf" Then
+                            brafText = GetPart(nextParts, 1)
+                            i += 1
+                        End If
+                    End If
+
+                    ' Remember the first Title's text so we can build the output filename.
+                    If documentTitleText = String.Empty Then
+                        documentTitleText = titledText
+                    End If
+
+                    sb.AppendLine("            <header class=""article-header"">")
+                    sb.AppendLine("                <h1 class=""headline"">" & titledText & "</h1>")
+                    sb.AppendLine("                <p class=""dek"">" & brafText & "</p>")
+                    sb.AppendLine("            </header>")
+
+                Case "Title Breaf"
+                    ' Standalone Title Breaf with no preceding Title item.
+                    sb.AppendLine("            <header class=""article-header"">")
+                    sb.AppendLine("                <h1 class=""headline""></h1>")
+                    sb.AppendLine("                <p class=""dek"">" & GetPart(parts, 1) & "</p>")
+                    sb.AppendLine("            </header>")
+
+                Case "figure"
+                    Dim srcText As String = GetPart(parts, 1)
+                    Dim altText As String = GetPart(parts, 2)
+                    Dim figcaptionText As String = GetPart(parts, 3)
+
+                    sb.AppendLine("            <figure>")
+                    sb.AppendLine("                <img src=""images/" & srcText & """ alt=""" & altText & """>")
+                    sb.AppendLine("                <figcaption>" & figcaptionText & "</figcaption>")
+                    sb.AppendLine("            </figure>")
+
+                Case "author"
+                    Dim autherText As String = GetPart(parts, 1)
+                    Dim datelineText As String = GetPart(parts, 2)
+                    Dim publineText As String = GetPart(parts, 3)
+
+                    sb.AppendLine("            <div class=""byline-block"">")
+                    sb.AppendLine("                <span class=""by"">By " & autherText & "</span>")
+                    If datelineText <> "" Then
+                        sb.AppendLine("                <span class=""dateline"">" & datelineText & "</span>")
+                    End If
+                    sb.AppendLine("                <span class=""pubdate"">" & publineText & "</span>")
+                    sb.AppendLine("            </div>")
+
+                Case "Paragraph"
+                    Dim paragraphText As String = GetPart(parts, 1)
+
+                    If Not articleBodyOpened Then
+                        sb.AppendLine("            <div class=""article-body"">")
+                        articleBodyOpened = True
+                    End If
+
+                    sb.AppendLine("<p>" & paragraphText & "</p>")
+
+                    If i = lastParagraphIndex Then
+                        sb.AppendLine("            </div>")
+                    End If
+
+                Case "author-bio"
+                    Dim bioNameText As String = GetPart(parts, 1)
+                    Dim infoText As String = GetPart(parts, 2)
+
+                    sb.AppendLine("            <div class=""author-bio"">")
+                    sb.AppendLine("                <p><strong>" & bioNameText & "</strong>" & infoText & "</p>")
+                    sb.AppendLine("            </div>")
+
+            End Select
+
+            i += 1
+        End While
+
+        Dim htmlOutput As String = sb.ToString()
+
+        Try
+            ' Assumes Template.txt lives in the App_Data folder - adjust the path below if it's elsewhere.
+            Dim templatePath As String = Server.MapPath("~/App_Data/Template.txt")
+
+            If Not System.IO.File.Exists(templatePath) Then
+                lblStatus.ForeColor = System.Drawing.Color.Red
+                lblStatus.Text = "Template.txt was not found at: " & templatePath
+                Exit Sub
+            End If
+
+            Dim templateText As String = System.IO.File.ReadAllText(templatePath)
+            Dim finalHtml As String = templateText.Replace("[[[[Article]]]]", htmlOutput)
+
+            Dim safeFileName As String = documentTitleText.Replace(" ", "") & ".html"
+            Dim outputPath As String = Server.MapPath("~/App_Data/" & safeFileName)
+
+            System.IO.File.WriteAllText(outputPath, finalHtml)
+
+            lblStatus.ForeColor = System.Drawing.Color.Green
+            lblStatus.Text = "Saved: " & safeFileName
+        Catch ex As Exception
+            lblStatus.ForeColor = System.Drawing.Color.Red
+            lblStatus.Text = "Error: " & ex.Message
+        End Try
+    End Sub
 End Class
